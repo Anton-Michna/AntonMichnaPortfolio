e6f19e43ee222254ca4447aed9d0a952210307ff
unknown <antonmichna@gmail.com>
2023-06-16 15:12:55 -0500

    Education section complete

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Education.css';
 import App from '../App';
 import { Slide } from '@material-ui/core';
+import UofM1 from '../Images/UofM1.jpg';
 
 function Education() {
     const [showApp, setShowApp] = useState(false);
@@ -18,18 +19,39 @@ function Education() {
 
   return (
 
-    <div className='child-component'>
+    <div className='education-child-component'>
       <Slide direction='right' in={showEdd} mountOnEnter unmountOnExit>
-          <div className='slide-container'>
-              <div className='heading-text'>
-                <h1>bonjour</h1>
-              </div>
+          <div className='education-container'>
+            <div className='education-heading-one'>
+              Education
+            </div>
 
-              
-              <div className='middle-link'>
+            <div className='education-heading-two'>
+              The University of Memphis
+            </div>
+
+            <div className='image-for-education'>
+              <img
+                src={UofM1}
+                alt='s'
+                style={{ width: '75vw', height: 'auto' }}
+              />
+            </div>
+
+            <div className='education-text-description'>
+            I graduated from The University of Memphis with a Baccalaureate degree in Computer Science in May of 2023. I completed this degree in 3 years, spanning from August 2020 to May 2023, with a Grade Point Average of 3.94. Relevant courses that I took include: Software Engineering, Computer Architecture, Data Structures and Algorithms, Programming Languages, Data Visualization, Operating Systems, Artificial Intelligence, 
+Data Mining, and Networking.
+            </div>
+         
+        
+
+
+              <div className='education-go-back-link'>
                 <button className='custom-button' onClick={handleGoBackClick}>Go Back</button>
               </div>
-            </div>
+
+          </div>
+            
       </Slide>
 
       {showApp && <App directionWhenCalled={'left'} />}
