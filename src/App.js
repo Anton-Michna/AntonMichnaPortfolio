cf62508e1fa3eb182beaa5bca2a53768239a0dad
unknown <antonmichna@gmail.com>
2023-06-16 17:00:47 -0500

    fixed spacing issues when page size was changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import './App.css';
+import './testapp.css';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import { Slide } from '@material-ui/core';
+import Anton from './anton.jpg';
 
 function App( {directionWhenCalled} ) {
   const [showEducation, setShowEducation] = useState(false);
@@ -37,36 +38,51 @@ function App( {directionWhenCalled} ) {
   };
 
   return (
-    <div className='child-component'>
-      <Slide direction={slideDirection} in={!isActive} mountOnEnter unmountOnExit>
-        <div className='slide-container'>
-          <div className={`profile-picture ${isActive ? 'slide-out' : ''}`}></div>
 
-          
-          <h1 style={{ fontSize: '60px', marginTop: '50px' }}>Anton Michna</h1>
+    <div className='app-component'>
+    <Slide direction={slideDirection} in={!isActive} mountOnEnter unmountOnExit>
+      <div className='app-to-contain'>
+        <div className='image-for-app'>
+          <img 
+            src={Anton}
+            alt='s'
+            style={{ width: 'auto', height: '55vh', borderRadius: '50%' }}
+          />
+        </div>
+
+        
+        <div className='app-first-heading'>
+              Anton Michna
+        </div>
 
 
-          
+        <div className='buttons-to-contain'>
 
-          <div className='right-link'>
-            <button className="custom-button" onClick={handleEducationClick}>Education</button>
-          </div>
+            <div className='right-link'>
+              <button className="custom-button" onClick={handleEducationClick}>Education</button>
+            </div>
 
-          <div className='left-link'>
-            <button className="custom-button" onClick={handleProjectsClick}>Projects</button>
-          </div>
+            <div className='middle-link'>
+              <button className="custom-button" onClick={handleSkillsClick}>Skills</button>
+            </div>
 
-          <div className='middle-link'>
-            <button className="custom-button" onClick={handleSkillsClick}>Skills</button>
-          </div>
+            <div className='left-link'>
+              <button className="custom-button" onClick={handleProjectsClick}>Projects</button>
+            </div>
+
 
         </div>
-      </Slide>
 
-      {showEducation && <Education />}
-      {showProjects && <Projects />}
-      {showSkills && <Skills />}
-    </div>
+      </div>
+    </Slide>
+
+    {showEducation && <Education />}
+    {showProjects && <Projects />}
+    {showSkills && <Skills />}
+  </div>
+
+
+
   );
 }
 
